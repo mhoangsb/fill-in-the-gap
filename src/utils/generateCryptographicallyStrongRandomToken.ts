b20b3a465903c77e7ade3709855e2033c61c9e45
mhoang <mhoangsb@gmail.com>
2025-03-27 09:59:25 +0700

    fix: change number of random bytes to generate match token

--- src/utils/generateCryptographicallyStrongRandomToken.ts
+++ src/utils/generateCryptographicallyStrongRandomToken.ts
@@ -2,7 +2,7 @@ import { randomBytes } from "node:crypto";
 
 export default async function generateCryptographicallyStrongRandomToken(): Promise<string> {
   return new Promise((resolve, reject) => {
-    randomBytes(48, function (err, buffer) {
+    randomBytes(32, function (err, buffer) {
       if (err) {
         reject(err);
       }
