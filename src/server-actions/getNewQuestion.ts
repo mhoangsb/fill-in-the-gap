6c947c50ad042a1a3230c6728b236fd5a0077b52
mhoang <mhoangsb@gmail.com>
2025-03-28 12:17:52 +0700

    feat: add getNewQuestion server action

---
+++ src/server-actions/getNewQuestion.ts
@@ -0,0 +1,88 @@
+"use server";
+
+import "server-only";
+
+import prisma from "@/utils/prisma";
+import { NULL_QUESTION_ID } from "@/utils/constants";
+import generateQuestion from "@/utils/generateQuestion";
+
+export type QuestionSendToClient = {
+  text: string;
+  author: string;
+  missingCharacterIndexes: number[];
+};
+
+enum GetNewQuestionErrorCode {
+  NoMatchFound,
+  InvalidHealth,
+  OngoingQuestion,
+}
+
+type ServerActionResult<T> =
+  | {
+      isOk: false;
+      errorMessage: string;
+      errorCode: number;
+    }
+  | {
+      isOk: true;
+      payload: T;
+    };
+
+export default async function getNewQuestion(
+  matchToken: string,
+): Promise<ServerActionResult<QuestionSendToClient>> {
+  const match = await prisma.match.findUnique({
+    where: {
+      cryptographicallyStrongRandomToken: matchToken,
+    },
+  });
+
+  if (!match) {
+    return {
+      isOk: false,
+      errorMessage: "Error: No match found with the given matchToken",
+      errorCode: GetNewQuestionErrorCode.NoMatchFound,
+    };
+  }
+
+  if (match.currentHealth <= 0) {
+    return {
+      isOk: false,
+      errorMessage: "Error: Invalid current health. Health must be above 0",
+      errorCode: GetNewQuestionErrorCode.InvalidHealth,
+    };
+  }
+
+  const isThereOngoingQuestion = match.currentQuestionId !== NULL_QUESTION_ID;
+  if (isThereOngoingQuestion) {
+    return {
+      isOk: false,
+      errorMessage: "Error: Match has ongoing question",
+      errorCode: GetNewQuestionErrorCode.OngoingQuestion,
+    };
+  }
+
+  const currentScore = match.currentScore;
+
+  const newQuestion = generateQuestion(currentScore);
+
+  await prisma.match.update({
+    where: {
+      cryptographicallyStrongRandomToken: matchToken,
+    },
+    data: {
+      currentQuestionId: newQuestion.quoteId,
+    },
+  });
+
+  return {
+    isOk: true,
+    payload: {
+      // Do not pass in newQuestion.quoteId
+      text: newQuestion.text,
+      author: newQuestion.author,
+      missingCharacterIndexes: newQuestion.missingCharacterIndexes,
+    },
+  };
+}
