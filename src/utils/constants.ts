dd2e5fc76a218034f6c05bed65a36ae653640890
mhoang <mhoangsb@gmail.com>
2025-04-01 23:51:33 +0700

    feat: add more constant values to constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,5 @@ export const HEALTH_PENALTY_WRONG_ANSWER = 1;
 export const SCORE_PER_HEALTH_BOOST = 3;
 export const HEALTH_BOOST = 1;
 export const NULL_QUESTION_ID = -1;
+export const HEALTH_COST_SHOW_ANSWER = 2;
+export const SCORE_PER_CORRECT_ANSWER = 1;
