3e16a3ac7ea5fb30f3d768c407c7c1e3cb734907
mhoang <mhoangsb@gmail.com>
2025-04-02 23:46:20 +0700

    feat: add Auth.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
 const inter = Inter({
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} dark bg-gray-950 text-gray-300 antialiased`}>
-        {children}
+        <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
   );
