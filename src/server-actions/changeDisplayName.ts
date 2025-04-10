bd05ada623af77bceed327e06456155ef6c6d618
mhoang <mhoangsb@gmail.com>
2025-04-10 23:19:31 +0700

    feat: add changeDisplayName server action

---
+++ src/server-actions/changeDisplayName.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { auth } from "@/auth";
+import prisma from "@/utils/prisma";
+import { ServerActionResult } from "@/utils/types";
+import { Prisma } from "@/../generated/prisma";
+
+enum ChangeDisplayNameErrorCode {
+  NotAuthenticated,
+  NameExisted,
+}
+
+export default async function changeDisplayName(
+  newName: string,
+): Promise<ServerActionResult<string>> {
+  const session = await auth();
+
+  if (!session) {
+    return {
+      isOk: false,
+      errorCode: ChangeDisplayNameErrorCode.NotAuthenticated,
+      errorMessage: "Not logged in yet",
+    };
+  }
+
+  if (!session.user?.email) {
+    throw new Error("Cannot obtain email after user has logged in");
+  }
+
+  try {
+    await prisma.user.update({
+      where: {
+        email: session.user.email,
+      },
+      data: {
+        displayName: newName,
+      },
+    });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2002") {
+      return {
+        isOk: false,
+        errorCode: ChangeDisplayNameErrorCode.NameExisted,
+        errorMessage: `Name "${newName}" existed`,
+      };
+    }
+
+    // There is also a case that prisma.user.update() throw error because
+    // user with the given email does not exist in the database
+    // but because I already add user to DB the first time they log in,
+    // I don't need to return an error code in this case
+    // If this case happen, my code is flawed and the error should be re-thrown
+
+    throw e;
+  }
+
+  return {
+    isOk: true,
+    payload: newName,
+  };
+}
