3a00abcf92956e8e07e6c45c02de24ae4b90c6a8
mhoang <mhoangsb@gmail.com>
2025-04-11 22:31:26 +0700

    fix: change SignOutButton style

--- src/app/(main)/profile/_componentns/SignOutButton.tsx
+++ src/app/(main)/profile/_componentns/SignOutButton.tsx
@@ -6,5 +6,12 @@ export default function SignOutButton() {
   const handleUserSignOut = () => {
     signOut();
   };
-  return <button onClick={handleUserSignOut}>Đăng xuất</button>;
+  return (
+    <button
+      className="cursor-pointer border-2 border-dashed border-red-900 px-6 py-2 hover:border-red-700"
+      onClick={handleUserSignOut}
+    >
+      Đăng xuất
+    </button>
+  );
 }
