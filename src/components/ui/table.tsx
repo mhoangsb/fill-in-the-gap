8bb84eb47976e0357bd3893bad816493e1bc7312
mhoang <mhoangsb@gmail.com>
2025-04-12 19:47:01 +0700

    fix: change table component's style

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -20,7 +20,7 @@ function TableHeader({ className, ...props }: React.ComponentProps<"thead">) {
   return (
     <thead
       data-slot="table-header"
-      className={cn("[&_tr]:border-b", className)}
+      className={cn("[&_tr]:border-b-2", className)}
       {...props}
     />
   );
@@ -51,7 +51,7 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
     <tr
       data-slot="table-row"
       className={cn(
-        "hover:bg-muted/50 data-[state=selected]:bg-muted border-b border-dashed transition-colors",
+        "hover:bg-muted/50 data-[state=selected]:bg-muted border-b-2 border-dashed transition-colors",
         className,
       )}
       {...props}
