0f537b7209b5cae2d0ee50c127c04f8a07c5163c
mhoang <mhoangsb@gmail.com>
2025-04-13 15:24:57 +0700

    feat: add loading.tsx for home page, profile page, and leaderboard page

---
+++ src/app/(main)/leaderboard/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingSpinner from "@/components/LoadingSpinner";
+
+export default function LeaderboardPageLoading() {
+  return (
+    <div className="mt-36 flex justify-center">
+      <LoadingSpinner />
+    </div>
+  );
+}
---
+++ src/app/(main)/profile/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingSpinner from "@/components/LoadingSpinner";
+
+export default function ProfilePageLoading() {
+  return (
+    <div className="mt-36 flex justify-center">
+      <LoadingSpinner />
+    </div>
+  );
+}
