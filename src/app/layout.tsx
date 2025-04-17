23fa61be857b16c91615d6766f817fdabf51552d
mhoang <mhoangsb@gmail.com>
2025-04-17 23:27:31 +0700

    fix: change html lang to vi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="vi">
       <body className={`${inter.className} dark bg-gray-950 text-gray-300 antialiased`}>
         <SessionProvider>{children}</SessionProvider>
       </body>
